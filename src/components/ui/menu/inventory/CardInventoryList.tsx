52afed640f4c0185db2ee166b056ef9c2fd27e8b
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-01-12 19:21:10 -0300

    Added Description to Card

--- src/components/ui/menu/inventory/CardInventoryList.tsx
+++ src/components/ui/menu/inventory/CardInventoryList.tsx
@@ -1,11 +1,12 @@
 import { Inventorys } from "../../../../types/types"
 
 
-export const CardInventoryList = ({id, title} : Inventorys) => {
+export const CardInventoryList = ({id, title, description} : Inventorys) => {
   return (
-    <div className="">
-        <h3>{title}</h3>
-        <p>id: {id}</p>
+    <div className="cardInventoryList">
+        <h3>{ title }</h3>
+        <p>id: { id }</p>
+        <p>{ description }</p>
     </div>
   )
 }
