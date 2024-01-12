fa4803a7ec0673a8009db7ff170df77fa2481f08
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-01-12 18:19:55 -0300

    Added Card for inventory list

--- src/components/menu/inventory/ListInventory.tsx
+++ src/components/menu/inventory/ListInventory.tsx
@@ -16,7 +16,7 @@ export const ListInventory = () => {
               
           </div>
         ))
-        : ''
+        : '' // !agregar skeleton
       }
     </div>
   )
---
+++ src/components/ui/menu/inventory/CardInventoryList.tsx
@@ -0,0 +1,9 @@
+
+export const CardInventoryList = () => {
+  return (
+    <div className="">
+        <h3>Title</h3>
+        <p>Description?</p>
+    </div>
+  )
+}
