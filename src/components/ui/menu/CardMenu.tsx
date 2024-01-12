a58adf701a5a6a14c0dfcb0067a8c36968263839
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-01-12 18:48:08 -0300

    Updating Cards Menu

--- src/components/ui/menu/CardMenu.tsx
+++ src/components/ui/menu/CardMenu.tsx
@@ -2,10 +2,12 @@ import { InventoryIcon } from "../../../assets/img/icons"
 
 export const CardMenu = () => {
   return (
-    <div className="CardMenu">
-      <h3>Inventory</h3>
-      <InventoryIcon/>
-      <p>View your inventory or create a new one</p>
-    </div>
+    <a href={'/inventory'}>
+      <div className="CardMenu">
+        <h3>Inventory</h3>
+        <InventoryIcon />
+        <p>View your inventory or create a new one</p>
+      </div>
+    </a>
   )
 }
