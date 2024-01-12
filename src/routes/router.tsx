87f6f01f826b09ca61b97560d4754294223460f1
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-01-12 17:49:04 -0300

    Added route Inventory List

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 import { Menu } from "../components/menu/Menu"
 import { App } from "../App"
+import { ListInventory } from "../components/menu/inventory/ListInventory"
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
             {
                 index: true,
                 element: <Menu/>
+            },
+            {
+                path: 'inventory',
+                element: <ListInventory/>
             },
             {
                 path: '*',
