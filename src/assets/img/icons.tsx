0fe0c4ff922b10944485ddef44ba0b3ac191e284
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-01-12 21:30:38 -0300

    Added new icon for button option

--- src/assets/img/icons.tsx
+++ src/assets/img/icons.tsx
@@ -1,3 +1,7 @@
 export const InventoryIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24"><path fill="#7c3aed" d="M20 2H4c-1 0-2 .9-2 2v3.01c0 .72.43 1.34 1 1.69V20c0 1.1 1.1 2 2 2h14c.9 0 2-.9 2-2V8.7c.57-.35 1-.97 1-1.69V4c0-1.1-1-2-2-2m-5 12H9v-2h6zm5-7H4V4h16z"/></svg>
 )
+
+export const OptionsIcon = ({color = '#000000'}: {color?: string} ) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={ color } strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
+)
