ce8ce0c52838edea5f605f900d4ca467b9c27913
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-01-12 21:33:09 -0300

    Added component ButtonOption

--- src/components/ui/menu/inventory/CardInventoryList.tsx
+++ src/components/ui/menu/inventory/CardInventoryList.tsx
@@ -1,12 +1,16 @@
 import { Inventorys } from "../../../../types/types"
+import { ButtonOption } from "../../buttonOption"
 
 
-export const CardInventoryList = ({id, title, description} : Inventorys) => {
-  return (
-    <div className="cardInventoryList">
-        <h3>{ title }</h3>
-        <p>id: { id }</p>
-        <p>{ description }</p>
-    </div>
-  )
+export const CardInventoryList = ({ id, title, description }: Inventorys) => {
+    return (
+        <div className="cardInventoryList">
+            <ButtonOption />
+            <div className="description">
+                <h3>{title}</h3>
+                <p>id: {id}</p>
+                <p>{description}</p>
+            </div>
+        </div>
+    )
 }
