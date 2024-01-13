d2e630fb4bb40421445e5ea269926627ba67605c
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-01-12 21:33:46 -0300

    Added and update css for ButtonOption.tsx

--- src/components/ui/ButtonOption.tsx
+++ src/components/ui/ButtonOption.tsx
@@ -1,6 +1,15 @@
+import { OptionsIcon } from "../../assets/img/icons"
 
 export const ButtonOption = () => {
-  return (
-    <div>soy un boton</div>
-  )
+    return (
+        <div className="optionButton">
+            <OptionsIcon />
+            <div className="dropDown">
+                <div className="options">
+                    <p className="edit">Edit</p>
+                    <p className="delete">Delete</p>
+                </div>
+            </div>
+        </div>
+    )
 }
