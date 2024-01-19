1b7606e7da6dd677ebdba9687b0530f6e8273c93
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-01-19 14:19:36 -0300

    Added And updated styles for dropdown, and use useDropDown

--- src/components/ui/ButtonOption.tsx
+++ src/components/ui/ButtonOption.tsx
@@ -1,13 +1,25 @@
+import { useRef, useState } from "react"
 import { OptionsIcon } from "../../assets/img/icons"
+import { useDropDown } from "../../hooks/useDropDown";
 
 export const ButtonOption = () => {
+
+    const  [ openOptions, setOpenOptions ]  = useState(false)
+    const ref = useRef<HTMLDivElement>(null);
+
+    useDropDown({ref, setOpenOptions})
+
+    const isActiveOptions = openOptions ? 'dropDown active' : 'dropDown'
+
     return (
-        <div className="optionButton">
-            <OptionsIcon />
-            <div className="dropDown">
+        <div className="optionButton" ref={ref}>
+            <div onClick={() => setOpenOptions(prev => !prev) }>
+                <OptionsIcon />
+            </div>
+            <div className={isActiveOptions}>
                 <div className="options">
-                    <p className="edit">Edit</p>
-                    <p className="delete">Delete</p>
+                    <a className="edit">Edit</a>
+                    <a className="delete">Delete</a>
                 </div>
             </div>
         </div>
