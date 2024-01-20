30b811410596a227c71e7947aeabf32b644c7447
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-01-20 13:01:11 -0300

    Added React-router '<Link>' to 'CardInventoryList.tsx'

--- src/components/ui/menu/inventory/CardInventoryList.tsx
+++ src/components/ui/menu/inventory/CardInventoryList.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { Inventorys } from "../../../../types/types"
 import { ButtonOption } from "../../ButtonOption"
 
@@ -6,11 +7,13 @@ export const CardInventoryList = ({ id, title, description }: Inventorys) => {
     return (
         <div className="cardInventoryList">
             <ButtonOption />
-            <div className="description">
-                <h3>{title}</h3>
-                <p>id: {id}</p>
-                <p>{description}</p>
-            </div>
+            <Link to={ `/inventory/id=${id}` }>
+                <div className="description">
+                    <h3>{title}</h3>
+                    <p>id: {id}</p>
+                    <p>{description}</p>
+                </div>
+            </Link>
         </div>
     )
 }
