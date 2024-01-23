c43050cd34b9cedfa1f38874e74d2cd6e0056ed4
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-01-23 17:52:34 -0300

    Added new component to inventory

--- src/components/menu/inventory/ViewInventory.tsx
+++ src/components/menu/inventory/ViewInventory.tsx
@@ -1,6 +1,18 @@
+import { ButtonOptionInventory } from "../../ui/menu/inventory/ButtonOptionInventory"
+
  
 export const ViewInventory = () => {
   return (
-    <div>ViewInventory</div>
+    <section className="section-inventory">
+      <div className="inventory-options">
+        <ButtonOptionInventory/>
+      </div>
+      <div className="inventory-location">
+
+      </div>
+      <div className="inventory-items">
+
+      </div>
+    </section>
   )
 }
