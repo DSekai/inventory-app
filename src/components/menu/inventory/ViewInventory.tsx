20d348a2e473144bd763536a839c661e1d05deca
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-01-24 17:30:45 -0300

    Added ButtonOptionInventory

--- src/components/menu/inventory/ViewInventory.tsx
+++ src/components/menu/inventory/ViewInventory.tsx
@@ -1,11 +1,19 @@
+// import { useRef, useState } from "react"
+import { ConfurationIcon } from "../../../assets/img/icons"
 import { ButtonOptionInventory } from "../../ui/menu/inventory/ButtonOptionInventory"
+import { ButtonOption } from "../../ui/ButtonOption"
 
  
 export const ViewInventory = () => {
   return (
     <section className="section-inventory">
       <div className="inventory-options">
-        <ButtonOptionInventory/>
+        <div className="options-buttons">
+          <ButtonOptionInventory/>
+        </div>
+        <div className="actions-button">
+          <ButtonOption title="Actions" options={['Delete']} image={<ConfurationIcon/>}/>
+        </div>
       </div>
       <div className="inventory-location">
 
