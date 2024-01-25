ff4b7e69514d3370af851c8071b59e6ac9a5fa1b
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-01-25 17:17:52 -0300

    Added new mock

--- src/components/menu/inventory/ViewInventory.tsx
+++ src/components/menu/inventory/ViewInventory.tsx
@@ -2,24 +2,45 @@
 import { ConfurationIcon } from "../../../assets/img/icons"
 import { ButtonOptionInventory } from "../../ui/menu/inventory/ButtonOptionInventory"
 import { ButtonOption } from "../../ui/ButtonOption"
+import { useReactTable } from "@tanstack/react-table"
+import data from '../../../libs/MOCK_DATA.json'
 
- 
+
 export const ViewInventory = () => {
+
+  useReactTable({ data, columns })
+
   return (
     <section className="section-inventory">
       <div className="inventory-options">
         <div className="options-buttons">
-          <ButtonOptionInventory/>
+          <ButtonOptionInventory />
         </div>
         <div className="actions-button">
-          <ButtonOption title="Actions" options={['Delete']} image={<ConfurationIcon/>}/>
+          <ButtonOption title="Actions" options={['Delete']} image={<ConfurationIcon />} />
         </div>
       </div>
       <div className="inventory-location">
 
       </div>
       <div className="inventory-items">
+        <table>
+          <thead>
+            <tr>
 
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+
+            </tr>
+          </tfoot>
+        </table>
       </div>
     </section>
   )
