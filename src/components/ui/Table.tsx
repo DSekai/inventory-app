57dadf45a91910a43c9dfddad655a50363d63030
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-04-02 20:39:21 -0300

    Update table

--- src/components/ui/Table.tsx
+++ src/components/ui/Table.tsx
@@ -1,22 +1,27 @@
 import { useState } from "react";
 import DataTable, { TableProps } from "react-data-table-component";
+import { DropDownComponent } from "./DropDownComponent";
+import { ConfurationIcon }  from "../../assets/img/icons"
+import { ProductType } from "../../types/types";
 
-export const Table = <T,>(props: TableProps<T>) => {
+export const Table = <T extends ProductType>(props: TableProps<T>) => {
 
-  const [selectedRows, setSelectedRows] = useState(false);
-  const [toggledClearRows, setToggleClearRows] = useState(false);
+  const [selectedRows, setSelectedRows] = useState<ProductType[] | [] >([]);
 
-  const handleChange = ({ selectedRows }) => {
-    setSelectedRows(selectedRows)
-    
+  const contextActions = (
+
+    <DropDownComponent data={selectedRows} options={[{key:'Edit'},{key:'Delete'}, {key: 'View'}]} title="Actions" image={<ConfurationIcon />}/>
+
+  )
+
+  const handleChange = (selected: { allSelected: boolean; selectedCount: number; selectedRows: T[] }) => {
+    setSelectedRows(selected.selectedRows)
   }
-
-  const handleClearRows = () => {
-    setToggleClearRows(!toggledClearRows)
-  }
-  console.log(selectedRows);
+  
 
   return (
-    <DataTable {...props} selectableRows onSelectedRowsChange={handleChange}/>
+    <>
+      <DataTable {...props} selectableRows onSelectedRowsChange={handleChange} contextActions={contextActions} />
+    </>
   )
 }
