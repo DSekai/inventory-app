58ae225a2a35fb48212567cf4b1c60d4108d44d8
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-04-09 04:21:21 -0400

    ♻️

--- src/components/ui/ButtonOption.tsx
+++ src/components/ui/ButtonOption.tsx
@@ -1,32 +1,32 @@
-import { useRef, useState } from "react"
-// import { OptionsIcon } from "../../assets/img/icons"
-import { useDropDown } from "../../hooks/useDropDown";
-import { ButtonOptionType } from "../../types/types";
+// import { useRef, useState } from "react"
+// // import { OptionsIcon } from "../../assets/img/icons"
+// import { useDropDown } from "../../hooks/useDropDown";
+// import { ButtonOptionType } from "../../types/types";
 
-export const ButtonOption = ({ title, options, image }:ButtonOptionType) => {
+// export const ButtonOption = ({ title, options, image }:ButtonOptionType) => {
 
-    const  [ openOptions, setOpenOptions ]  = useState(false)
-    const ref = useRef<HTMLButtonElement>(null);
+//     const  [ openOptions, setOpenOptions ]  = useState(false)
+//     const ref = useRef<HTMLButtonElement>(null);
 
-    useDropDown({ref, setOpenOptions})
+//     useDropDown({ref, setOpenOptions})
 
-    const isActiveOptions = openOptions ? 'dropDown active' : 'dropDown'
+//     const isActiveOptions = openOptions ? 'dropDown active' : 'dropDown'
 
-    return (
-        <button className="optionButton" ref={ref} onClick={() => setOpenOptions(prev => !prev) }>
-            <div className="titleButton" >
-                {image}
-                {title}
-            </div>
-            <div className={isActiveOptions}>
-                <div className="options">
-                    {
-                        options.map(option => (
-                            <a key={option} className="edit">{option}</a>
-                        ))
-                    }
-                </div>
-            </div>
-        </button>
-    )
-}
+//     return (
+//       <button className="optionButton" onClick={() => setOpenOptions(prev => !prev) } ref={ref}>
+//         <div className="titleButton" >
+//           {image}
+//           {title}
+//         </div>
+//         <div className={isActiveOptions}>
+//           <div className="options">
+//             {
+//                         options.map(option => (
+//                           <a className="edit" key={option}>{option}</a>
+//                         ))
+//                     }
+//           </div>
+//         </div>
+//       </button>
+//     )
+// }
