637a28f71ecbf61f939280d94a7a28376b81fad3
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-04-09 04:30:56 -0400

    :feat: Add useEffect for check token

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 
 // import { useEffect } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import { Layout } from './components/layouts/Layout'
+import { useBoundStore } from './store/bound.store'
+import { ModalLogin } from './components/ui/modals/ModalLogin'
 
 export const App = () => {
   // useEffect(() => {
@@ -21,8 +24,19 @@ export const App = () => {
   //     return () => mediaQueryList.removeEventListener('change', listener);
   //   }
   // }, []);
+  const token = window.localStorage.getItem('token')
+  const setType = useBoundStore(state => state.setType)
+  const onOpen = useBoundStore(state => state.onOpen)
+
+  useEffect(() => {
+    if(!token) {
+      setType('Login')
+      onOpen()
+    }
+  }, [])
   return (
     <>
+      <ModalLogin />
       <Layout/>
     </>
   )
