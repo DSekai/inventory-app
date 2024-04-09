6d98d1f481df3a8780ad93dd1db60eda019335f3
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-04-09 04:06:29 -0400

    Refactor

--- src/components/menu/inventory/ListInventory.tsx
+++ src/components/menu/inventory/ListInventory.tsx
@@ -1,22 +1,24 @@
 import inventory from '../../../libs/inventory.json'
 import { Inventorys } from '../../../types/types';
 import { CardInventoryList } from '../../ui/menu/inventory/CardInventoryList';
+import { CardSkeleton } from '../../ui/skeletons';
 
 export const ListInventory = () => {
 
-	const inventorysList: Inventorys[] = inventory
+  const inventorysList: Inventorys[] = inventory
 
-	return (
-  <>
-    <h2>Your Inventorys</h2>
-    <div className="inventoryList">
-      {
-					inventorysList.length !== 0 && inventorysList.map(inventory => (
-  <CardInventoryList description={inventory.description} id={inventory.id} key={inventory.id} title={inventory.title} />
-					))
-					// !agregar skeleton
-				}
-    </div>
-  </>
-	)
+  return (
+    <>
+      <h2>Your Inventorys</h2>
+      <div className="inventoryList">
+        {
+          inventorysList.length !== 0 ? inventorysList.map(inventory => (
+            <CardInventoryList description={inventory.description}
+              id={inventory.id} key={inventory.id} title={inventory.title}
+            />
+          )) : Array(3).fill(null).map((_,index) => <CardSkeleton key={index}/>)
+        }
+      </div>
+    </>
+  )
 }
