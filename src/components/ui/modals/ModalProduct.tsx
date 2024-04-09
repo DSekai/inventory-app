751b99f1f2907160f03c4d344e41cbb709a70cb8
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-04-09 04:17:18 -0400

    refactor

--- src/components/ui/modals/ModalProduct.tsx
+++ src/components/ui/modals/ModalProduct.tsx
@@ -1,14 +1,15 @@
 import { useBoundStore } from '../../../store/bound.store'
 import { Modal } from './Modal'
-import { Input } from '@nextui-org/react'
+import { Input, Select, SelectItem } from '@nextui-org/react'
+import { ModalFooterDefault } from './FooterModals/ModalFooterDefault'
 
 const custom = {
   
     inputWrapper: [
-      "h-[40px]"
+      "h-[47px]"
     ],
     label: [
-      "p-0"
+      // "py-1"
     ]
   
 }
@@ -18,19 +19,32 @@ export const ModalProduct = () => {
   const data = useBoundStore(state => state.data)
   const isOpen = useBoundStore(state => state.isOpen)
   const onClose = useBoundStore(state => state.onClose)
+  const modalType = useBoundStore(state => state.type)
 
+
+  const isOpenType = isOpen && modalType === 'Product'
 
   const dateFormated = data?.expiration_date.split(/[-/]/).reverse().join("-")
+
+  const category = [{id:'algo'}, {id:'otro'}, {id:'Clothing'}]
   
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal childrenFooter={<ModalFooterDefault onClose={onClose}/>} isOpen={isOpenType} onClose={onClose} size='xs' title={'Product'}>
       <div className="formContent">
         <Input classNames={custom} defaultValue={data?.product_name} label={'Name'} size="sm"  type={'text'} variant="bordered"/>
-        <Input classNames={custom} defaultValue={data?.price.toString()} label={'Price'} size="sm" type='number' variant="bordered"/>
-        <Input classNames={custom} defaultValue={data?.quantity.toString()} label={'Quantity'} size="sm" type={'text'} variant="bordered"/>
+        <div className="number">
+          <Input classNames={custom} defaultValue={data?.price.toString()} label={'Price'} size="sm" type='number' variant="bordered"/>
+          <Input classNames={custom} defaultValue={data?.quantity.toString()} label={'Quantity'} size="sm" type={'text'} variant="bordered"/>
+        </div>
         <Input classNames={custom} defaultValue={dateFormated} label={'Expire Date'} size="sm" type={'date'} variant="bordered"/>
-        <Input classNames={custom} defaultValue={'VER SI SE PUEDE PONER UNDROPDOWN'} label={'Name'} size="sm" type={'text'} variant="bordered"/>
-        <Input classNames={custom} defaultValue={data?.location} label={'Name'} size="sm" type={'text'} variant="bordered"/>
+        <Select defaultSelectedKeys={[data?.category ?? '']} label='Category' variant='bordered'>
+          {category.map((categ) => (
+            <SelectItem key={categ.id} value={categ.id}>
+              {categ.id}
+            </SelectItem>
+          ))}
+        </Select>
+        <Input classNames={custom} defaultValue={data?.location} label={'Location'} size="sm" type={'text'} variant="bordered"/>
       </div>
     </Modal>
   )
