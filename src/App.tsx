93de301776b762850d4928b6b7cce0231d46e51f
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-04-26 20:47:32 -0400

    Added Toaster

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,9 @@ import './App.css'
 import { Layout } from './components/layouts/Layout'
 import { useBoundStore } from './store/bound.store'
 import { ModalLogin } from './components/ui/modals/ModalLogin'
+import { Toaster } from 'sonner'
 
 export const App = () => {
-  // useEffect(() => {
-  //   const setBodyClass = (matches: boolean) => {
-  //     document.body.className = matches ? 'light text-foreground bg-background' : 'dark text-foreground bg-background';
-  //   }
-
-  //   if (typeof window !== 'undefined') {
-  //     const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
-  //     setBodyClass(mediaQueryList.matches);
-
-  //     const listener = (event: MediaQueryListEvent) => {
-  //       setBodyClass(event.matches);
-  //     }
-
-  //     mediaQueryList.addEventListener('change', listener);
-  //     return () => mediaQueryList.removeEventListener('change', listener);
-  //   }
-  // }, []);
   const token = window.localStorage.getItem('token')
   const setType = useBoundStore(state => state.setType)
   const onOpen = useBoundStore(state => state.onOpen)
@@ -38,6 +22,7 @@ export const App = () => {
     <>
       <ModalLogin />
       <Layout/>
+      <Toaster/>
     </>
   )
 }
