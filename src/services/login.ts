a518f9ba9e0c8acfb4ea71088b391aa2753c2797
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-04-26 20:46:26 -0400

    updated response to Api

--- src/services/login.ts
+++ src/services/login.ts
@@ -2,7 +2,7 @@ import { inventoryAPI } from "../api/inventory.api";
 import { handleErrorExepcion } from "../common/utils/handleErrors";
 import { LoginResponseType, LoginType } from "../types/types";
 
-export const loginApi = (payLoad: LoginType) => inventoryAPI.post<LoginResponseType>('/auth/login', {data: payLoad})
+export const loginApi = (payLoad: LoginType) => inventoryAPI.post<LoginResponseType>('/auth/login', payLoad)
   .then(response => {
     return response.data
   }).catch(error => {
