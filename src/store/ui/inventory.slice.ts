9ae3731c0221a19e2fdb6568c3ed274da57b35a0
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-05-03 01:14:28 -0400

    update and added news functions

--- src/store/ui/inventory.slice.ts
+++ src/store/ui/inventory.slice.ts
@@ -1,25 +1,51 @@
 import { StateCreator } from 'zustand'
 import { InventoryType } from '../../types/types'
 
+type data = InventoryType | null
+
 interface InventoryState {
   inventories: InventoryType[] | []
+  inventoryData: data | null
 }
 interface InventoryFunctionState {
   setInventory: (inventory: InventoryType) => void
+  deleteInventory: (inventoryToDelete: InventoryType) => void
+  updateInventory: (inventoryToUpdate: InventoryType) => void
   setInventories: (inventories: InventoryType[]) => void
+  setInventoryData: (data: data) => void
 }
 export type InventoryStateSliceType = InventoryState & InventoryFunctionState
 
 const initialState: InventoryState = {
   inventories: [],
+  inventoryData: null
 }
-export const createInventorySlice : StateCreator<InventoryState & InventoryFunctionState> = (set) => ({
+export const createInventorySlice: StateCreator<InventoryState & InventoryFunctionState> = (set, get) => ({
   ...initialState,
   setInventory(inventory) {
-    set(state => ({inventories:[...state.inventories, {...inventory}]}))
+    set(state => ({ inventories: [...state.inventories, { ...inventory }] }))
   },
-
+  deleteInventory(inventoryToDelete) {
+    set(state => ({
+      inventories: state.inventories.filter((inventory) => inventory.id !== inventoryToDelete.id),
+    }))
+  },
+  updateInventory(inventoryToUpdate) {
+    const inventoryFound = get().inventories.find(inventory => inventory.id === inventoryToUpdate.id)
+    if (!inventoryFound) return
+    set(state => ({
+      ...state, inventories: state.inventories.map(inventory => {
+        if (inventory.id === inventoryFound.id) {
+          return { ...inventory, ...inventoryToUpdate }
+        }
+        return inventory
+      })
+    }))
+  },
   setInventories(inventories) {
-    set({inventories})
+    set({ inventories })
+  },
+  setInventoryData(inventoryData) {
+    set({ inventoryData })
   },
 })
