9d2ef12672ae51f5d957c3f9eded4b19a9e36fb6
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-05-03 01:06:19 -0400

    change add new inventory for add inventory

--- src/components/ui/ButtonAdd.tsx
+++ src/components/ui/ButtonAdd.tsx
@@ -14,7 +14,7 @@ export const ButtonAdd = () => {
   return (
     <div className="flex gap-4 items-center fixed bottom-12 z-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
       <Button className='text-white' color="success" endContent={<PlusIcon/>} onPress={handleAction} size='lg'>
-        Add new Inventory
+        Add Inventory
       </Button>    
     </div>
   )
