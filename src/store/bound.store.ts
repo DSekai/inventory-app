0e18af16aba5ebd6810abf83692dfc636f008bfd
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-05-03 01:09:13 -0400

    update

--- src/store/bound.store.ts
+++ src/store/bound.store.ts
@@ -1,9 +1,10 @@
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
-import { ModalType } from '../types/types'
+import { ModalType, ProductModalType } from '../types/types'
 import { createUiSlice } from './ui/ui.slice'
 import { UserStateSliceType, createUserSlice } from './ui/user.slice'
 import { InventoryStateSliceType, createInventorySlice } from './ui/inventory.slice'
+import { createProductSlice } from './ui/product.slice'
 
 // import { createAuthSlice, type AuthSlice } from './ui/ui.slice'
 
@@ -11,11 +12,12 @@ import { InventoryStateSliceType, createInventorySlice } from './ui/inventory.sl
 //   ...createAuthSlice(...a)
 // }))
 
-type Slices = ModalType & UserStateSliceType & InventoryStateSliceType
+type Slices = ModalType & UserStateSliceType & InventoryStateSliceType & ProductModalType
 
 export const useBoundStore = create<Slices>()(
   devtools(
     (...a) => ({
+      ...createProductSlice(...a),
       ...createInventorySlice(...a),
       ...createUserSlice(...a),
       ...createUiSlice(...a)
