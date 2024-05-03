d30116dd5c6e6fdeaf12d6d7699d536640734a22
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-05-03 01:08:01 -0400

    added new gunction update and cllaed news functions of bondStore

--- src/hooks/useInventories.ts
+++ src/hooks/useInventories.ts
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { getInventoriesAPI, postInventoriesAPI } from "../services/inventorys";
+import { desactiveInventoryAPI, getInventoriesAPI, patchInventoryApi, postInventoriesAPI } from "../services/inventorys";
 import { InventoryType } from "../types/types";
 import { useAuth } from "./useAuth";
 import { useBoundStore } from "../store/bound.store";
@@ -11,6 +11,8 @@ export function useInventories () {
     const addInventory = useBoundStore(state => state.setInventory)
     const inventories = useBoundStore(state => state.inventories)
     const setInventories = useBoundStore(state => state.setInventories)
+    const deleteInventories = useBoundStore(state => state.deleteInventory)
+    const updateInventories = useBoundStore(state => state.updateInventory)
 
     const getInventory = async () => {
         await getInventoriesAPI()
@@ -24,13 +26,28 @@ export function useInventories () {
             .catch(error => {throw error})
     }
 
+    const deleteInventory = async (inventoryID: InventoryType['id']) => {
+        await desactiveInventoryAPI(inventoryID)
+            .then(res => deleteInventories(res))
+            .catch(err => {throw err})
+    }
+
+    const updateInventory = async (payLoad: InventoryType) => {
+        await patchInventoryApi(payLoad)
+            .then(res => {
+                updateInventories(res)
+            })
+    }
+
     useEffect(() => {
         getInventory()
     },[userState])
 
     return {
         inventories,
-        postInventory
+        postInventory,
+        deleteInventory,
+        updateInventory
     }
     
 }
