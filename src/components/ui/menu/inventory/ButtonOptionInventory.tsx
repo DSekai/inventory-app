5612b5f93eb9c96377e202c2de7e71a3feb856b7
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-05-03 01:03:47 -0400

    update params

--- src/components/ui/menu/inventory/ButtonOptionInventory.tsx
+++ src/components/ui/menu/inventory/ButtonOptionInventory.tsx
@@ -2,8 +2,8 @@
 export const ButtonOptionInventory = () => {
   return (
     <>
-        <button>New</button>
-        <button>Order</button>
+      <button>New</button>
+      <button>Order</button>
     </>
   )
 }
--- src/components/ui/menu/inventory/CardInventoryList.tsx
+++ src/components/ui/menu/inventory/CardInventoryList.tsx
@@ -3,20 +3,23 @@ import { OptionsIcon } from "../../../../assets/img/icons"
 import { DropDownComponent } from "../../DropDownComponent"
 import { InventoryType } from "../../../../types/types"
 
+interface Props {
+  inventory: InventoryType
+}
 
-export const CardInventoryList = ({ id, name, description }: InventoryType) => {
-
+export const CardInventoryList = ({inventory}:Props ) => {
+    
     return (
-      <Link to={`/inventory/${id}`} >
+      <Link to={`/inventory/${inventory.id}`} >
         <div className="cardInventoryList">
           <header className="">
             <div className="">
-              <DropDownComponent data={null} image={<OptionsIcon />} options={[{ key: 'Edit' }, { key: 'Delete' }]} sizeButton="md" type="Inventory" />
+              <DropDownComponent data={inventory} image={<OptionsIcon />} options={[{ key: 'View' }, { key: 'Delete' }]} sizeButton="md" type="Inventory" />
             </div>
           </header>
           <div className="description">
-            <h2 className="title">{name}</h2>
-            <p>{description}</p>
+            <h2 className="title">{inventory.name}</h2>
+            <p>{inventory.description}</p>
           </div>
         </div>
       </Link>
