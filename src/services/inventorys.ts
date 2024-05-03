cf84292dbd649b6928688262e4ce057cb63bb169
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-05-03 01:08:37 -0400

    add new function patchInventoryApi

--- src/services/inventorys.ts
+++ src/services/inventorys.ts
@@ -12,3 +12,11 @@ export const getInventoriesAPI = () => inventoryAPI.get<InventoryType[]>('/inven
 export const postInventoriesAPI = (payLoad:InventoryType) => inventoryAPI.post('inventories', payLoad)
   .then(response => (response.data))
   .catch(error => {throw handleErrorExepcion(error)})
+
+export const desactiveInventoryAPI = (inventoryID: InventoryType['id']) => inventoryAPI.patch(`inventories/delete/${inventoryID}`)
+  .then(res => (res.data))
+  .catch(error => {throw handleErrorExepcion(error)})
+
+export const patchInventoryApi = (payLoad: InventoryType) => inventoryAPI.patch('inventories', payLoad)
+  .then(res => (res.data))
+  .catch(err => {throw handleErrorExepcion(err)})
